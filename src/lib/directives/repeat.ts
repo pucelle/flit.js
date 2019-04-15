8f28a743bc93978f89dd190e73c602738e8e38ed
Freeman Chen <pucelle@163.com>
2019-04-15 22:13:48 +0800

    Now each item in `repeat` was updated independently.

--- src/lib/directives/repeat.ts
+++ src/lib/directives/repeat.ts
@@ -2,30 +2,59 @@ import {defineDirective, Directive, DirectiveResult} from './define'
 import {TemplateResult, Template} from '../parts'
 import {text} from '../parts/template-result'
 import {Transition, TransitionOptions, formatShortTransitionOptions, ShortTransitionOptions} from '../transition'
+import {Watcher} from '../watcher'
+import {observe} from '../observer';
 
 
 type TemplateFn<T> = (item: T, index: number) => TemplateResult | string
 
 
-export const repeat = defineDirective(class RepeatDirective<T> extends Directive {
+class RepeatDirective<T> extends Directive {
 
 	private items: T[] = []
-	private templates: Template[] = []
-	private templateFn: TemplateFn<T> | null = null
+	private wtems: WatchedTemplate<T>[] = []
 	private transitionOptions: TransitionOptions | null = null
+	private itemsWatcher: Watcher<T[]> | null = null
+
+	templateFn: TemplateFn<T> | null = null
 
 	init(items: Iterable<T>, templateFn: TemplateFn<T>, transitionOptions?: ShortTransitionOptions) {
-		this.items = items ? [...items] : []
+		this.items = this.getItems(items)
 		this.templateFn = templateFn
 		
 		let index = 0
-		for (let item of this.items!) {
+		for (let item of this.items) {
 			let template = this.createTemplate(item, index, null)
-			this.templates.push(template)
+			this.wtems.push(template)
 		}
 
 		// Doesn't play transition for the first time
 		this.initTransitionOptions(transitionOptions)
+	}
+
+	private getItems(items: Iterable<T>): T[] {
+		if (this.itemsWatcher) {
+			this.itemsWatcher.disconnect()
+		}
+
+		let watchFn = () => {
+			// items got here may be not been observed currently, event been updated here.
+			// But they will becomes observed ones when updated from outside.
+			// So here we just observe them all.
+			let observedItems: T[] = []
+			for (let item of items) {
+				observedItems.push(observe(item))
+			}
+			return observedItems
+		}
+
+		let onUpdate = (items: T[]) => {
+			this.updateItems(items)
+		}
+
+		this.itemsWatcher = new Watcher(watchFn, onUpdate)
+
+		return this.itemsWatcher.value 
 	}
 
 	private initTransitionOptions(transitionOptions: ShortTransitionOptions | undefined) {
@@ -37,20 +66,14 @@ export const repeat = defineDirective(class RepeatDirective<T> extends Directive
 		}
 	}
 
-	private createTemplate(item: T, index: number, nextNode: ChildNode | null): Template {
-		let result = this.templateFn!(item, index++)
-		if (typeof result === 'string') {
-			result = text`${result}`
-		}
-
-		let template = new Template(result, this.context)
-		let fragment = template.getFragment()
+	private createTemplate(item: T, index: number, nextNode: ChildNode | null): WatchedTemplate<T> {
+		let wtem = new WatchedTemplate(this, item, index)
+		let template = wtem.template!
+		let fragment = template!.getFragment()
+		let firstElement: HTMLElement | null = null
 
 		if (this.transitionOptions) {
-			let firstElement = fragment.firstElementChild as HTMLElement
-			if (firstElement) {
-				new Transition(firstElement, this.transitionOptions).enter()
-			}
+			firstElement = fragment.firstElementChild as HTMLElement
 		}
 
 		if (nextNode) {
@@ -60,11 +83,20 @@ export const repeat = defineDirective(class RepeatDirective<T> extends Directive
 			this.anchorNode.before(fragment)
 		}
 
-		return template
+		if (firstElement) {
+			new Transition(firstElement, this.transitionOptions!).enter()
+		}
+
+		return wtem
 	}
 
 	canMergeWith(_items: Iterable<T>, templateFn: TemplateFn<T>): boolean {
 		return templateFn.toString() === this.templateFn!.toString()
+	}
+
+	merge(items: Iterable<T>, _templateFn: TemplateFn<T>, transitionOptions: ShortTransitionOptions) {
+		this.initTransitionOptions(transitionOptions)
+		this.updateItems(this.getItems(items))
 	}
 
 	// We want to reduce moving times, the best way is here:
@@ -77,19 +109,17 @@ export const repeat = defineDirective(class RepeatDirective<T> extends Directive
 	// But here we need to keep the index of template nodes that will be removed,
 	// So we check from start position to end position,
 	// collected templates which will be removed but keep them in their old position.
-	merge(items: Iterable<T>, _templateFn: TemplateFn<T>, transitionOptions: ShortTransitionOptions) {
-		
+	updateItems(items: T[]) {
 		// Old
 		let oldItems = this.items
 		let oldItemIndexMap: Map<T, number> = new Map()
-		let oldTemplates = this.templates
+		let oldWtems = this.wtems
 		
 
 		// New
-		let newItems = this.items = items ? [...items] : []
+		let newItems = this.items = items
 		let newItemSet: Set<T> = new Set(this.items)
-		let newTemplates: Template[] = this.templates = []
-		this.initTransitionOptions(transitionOptions)
+		let newWtems: WatchedTemplate<T>[] = this.wtems = []
 
 		
 		// Handle removing and reusing
@@ -122,9 +152,11 @@ export const repeat = defineDirective(class RepeatDirective<T> extends Directive
 					oldIndex++
 				}
 
-				// Old item ust in the right position, nothing need to do
+				// Old item just in the right position, only update index
 				if (item === oldItems[oldIndex]) {
-					newTemplates.push(oldTemplates[oldIndex])
+					let wtem = oldWtems[oldIndex]
+					wtem.updateIndex(index)
+					newWtems.push(wtem)
 					reusedIndexSet.add(oldIndex)
 					oldIndex++
 					continue
@@ -139,11 +171,13 @@ export const repeat = defineDirective(class RepeatDirective<T> extends Directive
 				}
 
 				if (reusedIndex > -1) {
-					let template = oldTemplates[reusedIndex]
+					let wtem = oldWtems[reusedIndex]
+					let template = wtem.template!
 
 					// No need to check if `reusedIndex === oldIndex`, they are not equal
-					this.moveTemplate(template, oldIndex < oldItems.length ? oldTemplates[oldIndex].startNode : null)
-					newTemplates.push(template)
+					this.moveTemplate(template, oldIndex < oldItems.length ? oldWtems[oldIndex].template!.startNode : null)
+					wtem.updateIndex(index)
+					newWtems.push(wtem)
 					reusedIndexSet.add(reusedIndex)
 					continue
 				}
@@ -152,22 +186,23 @@ export const repeat = defineDirective(class RepeatDirective<T> extends Directive
 			// Reuse template that will be removed and rerender it
 			if (willRemoveIndexSet.size > 0 && !this.transitionOptions) {
 				let reusedIndex = willRemoveIndexSet.keys().next().value
-				let template = oldTemplates[reusedIndex]
+				let wtem = oldWtems[reusedIndex]
+				let template = wtem.template!
 
-				this.moveTemplate(template, oldIndex < oldItems.length ? oldTemplates[oldIndex].startNode : null)
-				this.reuseTemplate(template, item, index)
-				newTemplates.push(template)
+				this.moveTemplate(template, oldIndex < oldItems.length ? oldWtems[oldIndex].template!.startNode : null)
+				wtem.update(item, index)
+				newWtems.push(wtem)
 				reusedIndexSet.add(reusedIndex)
 				continue
 			}
 
-			newTemplates.push(this.createTemplate(item, index, oldIndex < oldItems.length ? oldTemplates[oldIndex].startNode : null))
+			newWtems.push(this.createTemplate(item, index, oldIndex < oldItems.length ? oldWtems[oldIndex].template!.startNode : null))
 		}
 
 		if (reusedIndexSet.size < oldItems.length) {
 			for (let i = 0; i < oldItems.length; i++) {
 				if (!reusedIndexSet.has(i)) {
-					this.removeTemplate(oldTemplates[i])
+					this.removeTemplate(oldWtems[i])
 				}
 			}
 		}
@@ -184,34 +219,99 @@ export const repeat = defineDirective(class RepeatDirective<T> extends Directive
 		}
 	}
 
-	private reuseTemplate(template: Template, item: T, index: number) {
-		let result = this.templateFn!(item, index++)
-		if (typeof result === 'string') {
-			result = text`${result}`
-		}
-		template.merge(result)
-	}
+	private removeTemplate(wtem: WatchedTemplate<T>) {
+		let template = wtem.template!
 
-	private removeTemplate(template: Template) {
 		if (this.transitionOptions) {
 			let firstElement = template.getNodes().find(el => el.nodeType === 1) as HTMLElement | undefined
 			if (firstElement) {
 				new Transition(firstElement, this.transitionOptions).leave(() => {
-					template.remove()
+					wtem.remove()
 				})
 			}
 			else {
-				template.remove()
+				wtem.remove()
 			}
 		}
 		else {
-			template.remove()
+			wtem.remove()
 		}
 	}
 
 	remove() {
-		for (let template of this.templates) {
+		for (let template of this.wtems) {
 			template.remove()
 		}
 	}
-}) as <T>(items: Iterable<T>, templateFn: TemplateFn<T>, transitionOptions?: ShortTransitionOptions) => DirectiveResult
+}
+
+export const repeat = defineDirective(RepeatDirective) as <T>(items: Iterable<T>, templateFn: TemplateFn<T>, transitionOptions?: ShortTransitionOptions) => DirectiveResult
+
+
+class WatchedTemplate<T> {
+
+	private directive: RepeatDirective<T>
+	private item: T
+	private index: number
+	private watcher: Watcher<TemplateResult> | null = null
+
+	template: Template
+
+	constructor(directive: RepeatDirective<T>, item: T, index: number) {
+		this.directive = directive
+		this.item = item
+		this.index = index
+		this.template = this.getTemplate()
+	}
+
+	private getTemplate(): Template {
+		let templateFn = this.directive.templateFn!
+		let context = this.directive.context
+
+		let watchFn = () => {
+			let result = templateFn(this.item, this.index)
+			if (typeof result === 'string') {
+				result = text`${result}`
+			}
+			return result
+		}
+	
+		let onUpdate = (result: TemplateResult) => {
+			// Note that the template update in the watcher updating queue.
+			if (this.template.canMergeWith(result)) {
+				this.template.merge(result)
+			}
+			else {
+				let newTemplate = new Template(result, context)
+				this.template.startNode.before(newTemplate.getFragment())
+				this.template.remove()
+				this.template = newTemplate
+			}
+		}
+	
+		this.watcher = new Watcher(watchFn, onUpdate)
+
+		return new Template(this.watcher.value, context)
+	}
+
+	updateIndex(index: number) {
+		if (index !== this.index) {
+			this.index = index
+			this.watcher!.__updateImmediately()
+		}
+	}
+
+	update(item: T, index: number) {
+		if (item !== this.item || index !== this.index) {
+			this.item = item
+			this.index = index
+			this.watcher!.__updateImmediately()
+		}
+	}
+
+	remove() {
+		this.template!.remove()
+		this.watcher!.disconnect()
+	}
+}
+
