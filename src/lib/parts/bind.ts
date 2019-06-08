ca316c115d661f824f6b4a234ca4d24abf0ad26e
Freeman Chen <pucelle@163.com>
2019-06-08 17:00:45 +0800

    Now using `.property` to assign com properties

--- src/lib/parts/bind.ts
+++ src/lib/parts/bind.ts
@@ -1,6 +1,6 @@
 import {MayStringValuePart, PartType} from './shared'
 import {Binding, getDefinedBinding} from '../bindings'
-import {Context, Component, getComponent, onComponentCreatedAt} from '../component'
+import {Context} from '../component'
 
 
 /**
@@ -12,70 +12,22 @@ export class BindingPart implements MayStringValuePart {
 	type: PartType = PartType.Binding
 	strings: string[] | null = null
 
-	private binding: Binding | null = null
-	private property: string = ''
-	private com: Component | null = null
-	private value: unknown = undefined
+	private binding: Binding
 
 	constructor(el: Element, name: string, value: unknown, context: Context) {
 		let dotIndex = name.indexOf('.')
 		let bindingName = dotIndex > -1 ? name.slice(0, dotIndex) : name
 		let bindingModifiers = dotIndex > -1 ? name.slice(dotIndex + 1).split('.') : null
-		let isPropertyBinding = bindingName[0] === ':'
-		let BindingClass: any
+		let BindingClass = getDefinedBinding(bindingName)
 
-		if (isPropertyBinding) {
-			bindingName = bindingName.slice(1)
+		if (!BindingClass) {
+			throw new Error(`":${name}" on <${el.localName}> is not a registered binding class`)
 		}
 
-		if (!isPropertyBinding && (BindingClass = getDefinedBinding(bindingName))) {
-			this.binding = new BindingClass(el, value, bindingModifiers, context)
-		}
-		else {
-			this.bindComProperty(el, bindingName)
-			this.updateComProperty(value)
-		}
-	}
-
-	private bindComProperty(el: Element, property: string) {
-		this.property = property
-
-		let com = getComponent(el as HTMLElement)
-		if (com) {
-			this.com = com
-		}
-		else {
-			onComponentCreatedAt(el as HTMLElement, this.onComCreated.bind(this))
-		}
-	}
-
-	private onComCreated(com: Component) {
-		this.com = com
-		this.setComProperty(this.value)
-		this.value = undefined	
+		this.binding = new BindingClass(el, value, bindingModifiers, context)
 	}
 
 	update(value: unknown) {
-		if (this.binding) {
-			this.binding.update(value)
-		}
-		else {
-			this.updateComProperty(value)
-		}
-	}
-
-	private updateComProperty(value: unknown) {
-		if (this.com) {
-			this.setComProperty(value)
-		}
-		else {
-			this.value = value
-		}
-	}
-
-	setComProperty(value: unknown) {
-		// We did compare values in component value setting to trigger update,
-		// Such that here no need compare again.
-		(this.com as any)[this.property] = value
+		this.binding.update(value)
 	}
 }
--- src/lib/parts/property.ts
+++ src/lib/parts/property.ts
@@ -1,9 +1,12 @@
 import {MayStringValuePart, PartType} from './shared'
+import {Component, getComponent, onComponentCreatedAt} from '../component'
 
 
 /**
- * `.property=${...}`, will assign value to element by `element.property = value`.
- */
+ * `.property=${...}` will assign value to element by `element.property = value`.
+ * `.comProperty=${...}` will assign value to component by `com.property = value`.
+ * `..property=${...}` to always assign to element.
+*/
 export class PropertyPart implements MayStringValuePart {
 
 	type: PartType = PartType.Property
@@ -11,14 +14,54 @@ export class PropertyPart implements MayStringValuePart {
 
 	private el: Element
 	private name: string
+	private com: Component | null = null
+	private isComProperty: boolean
+	private value: unknown = undefined
 
 	constructor(el: Element, name: string, value: unknown) {
 		this.el = el
-		this.name = name
-		this.setValue(value)
+		this.name = name[0] === '.' ? name.slice(1) : name
+		this.isComProperty = el.localName.includes('-') && name[0] !== '.'
+
+		if (this.isComProperty) {
+			this.bindCom()
+			this.updateComProperty(value)
+		}
+		else {
+			this.updateElementProperty(value)
+		}
 	}
 
-	private setValue(value: unknown) {
+	private bindCom() {
+		let com = getComponent(this.el as HTMLElement)
+		if (com) {
+			this.com = com
+		}
+		else {
+			onComponentCreatedAt(this.el as HTMLElement, this.onComCreated.bind(this))
+		}
+	}
+
+	private onComCreated(com: Component) {
+		this.com = com
+		this.setComProperty(this.value)
+		this.value = undefined	
+	}
+
+	private updateComProperty(value: unknown) {
+		if (this.com) {
+			this.setComProperty(value)
+		}
+		else {
+			this.value = value
+		}
+	}
+
+	private setComProperty(value: unknown) {
+		(this.com as any)[this.name] = value
+	}
+
+	private updateElementProperty(value: unknown) {
 		// Required, set same value for `<input type="text">` may cause cursor position reset.
 		if ((this.el as any)[this.name] !== value) {
 			(this.el as any)[this.name] = value
@@ -26,7 +69,12 @@ export class PropertyPart implements MayStringValuePart {
 	}
 
 	update(value: unknown) {
-		this.setValue(value)
+		if (this.isComProperty) {
+			this.updateComProperty(value)
+		}
+		else {
+			this.updateComProperty(value)
+		}
 	}
 
 	remove() {}
